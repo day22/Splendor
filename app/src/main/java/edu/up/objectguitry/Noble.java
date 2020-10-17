46196e6cfe8d7433e99bf4c863e9c0a6d1f4d7e7
brownr425 <brown-ryan@comcast.net>
2020-10-17 15:16:00 -0700

    run test xml

--- app/src/main/java/edu/up/objectguitry/Noble.java
+++ app/src/main/java/edu/up/objectguitry/Noble.java
@@ -1,4 +1,11 @@
 package edu.up.objectguitry;
 
 public class Noble {
+    private int rPrice;
+    private int bPrice;
+    private int gPrice;
+    private int wPrice;
+    private int brPrice;
+
+    private int prestigePoints;
 }
